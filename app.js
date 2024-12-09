ed8264237c226dc1a9d96db0dede8295612f0ef1
Adedeji-Kehinde <129548218+Adedeji-Kehinde@users.noreply.github.com>
2024-12-09 17:26:24 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 // app.js
 const express = require("express");
+const path = require("path");
+const checkout = require("./routes/checkout");
+const { json, urlencoded } = require("body-parser");
+
 const app = express();
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/checkout", checkout);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(index.html);
 });
 
 app.listen(3000, () => {
   console.log("App listening on port 3000");
 });
+
+module.exports = app
